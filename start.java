f1a55e79b0fd1f31c932b975691186f412e3f201
Johnson-nifemi <130283903+Johnson-nifemi@users.noreply.github.com>
2024-05-14 10:59:24 +0100

    Add files via upload

---
+++ start.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class HelloWorld {
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+
+    String username = "janeDoe";
+    String Password = "password";
+
+    String login(){
+      if(username.equals("janeDoe")&& Password.equals("password"){
+        System.out.println("success"));
+      }
+      else()
+
+      int getUserAge(int yearOfBirth){
+        int age  = 2004 - 2024;
+        System.out.println(age);
+ 
+        // inheritance, encapsulaton& polymorphism
+        // interfaces & method overriding
+        // data structures array, stack & queues
+        // generics; lists, sets, maps
+        // java swing library --------> file water
+        // exception handling
+        // streams API
+      }
+    }
+    
+
+    //System.out.println(Password);
+    //System.out.println("Hello, World!");
+  }
+}
